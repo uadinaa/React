5db98c52cf960ca00d953a09db3f1eaace6ff5f2
Abitova Dina <123065685+uadinaa@users.noreply.github.com>
2025-11-13 16:53:11 +0500

    routing added

---
+++ routing/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        < div>
+            <h1>Login Page</h1>
+            <h2>Auth to be added</h2>
+        </div>
+    );
+
+}
+
+export default Login;
