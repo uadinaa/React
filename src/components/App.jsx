12bc2dc9f5ef3f7b336b846cc792cf5f202359e3
Abitova Dina <123065685+uadinaa@users.noreply.github.com>
2025-11-27 01:17:02 +0500

    App
    Integrate Firebase authentication state management and update user state in Redux.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,42 @@
+// import { Outlet } from "react-router-dom";
+// import Header from "./Header.jsx";
+
+// export default function App() {
+//     return (
+//         <div>
+//             <Header />
+//             <main style={{ padding: "20px" }}>
+//                 <Outlet /> {}
+//             </main>
+//         </div>
+//     );
+// }
+
+
+
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+import { useDispatch } from "react-redux";
+import { setUser } from "../slices/authSlice";
 
 export default function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (user) => {
+            dispatch(setUser(user ? { uid: user.uid, email: user.email } : null));
+        });
+        return () => unsub();
+    }, [dispatch]);
+
     return (
         <div>
             <Header />
             <main style={{ padding: "20px" }}>
-                <Outlet /> {}
+                <Outlet />
             </main>
         </div>
     );
