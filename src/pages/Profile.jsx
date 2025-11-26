d1f02871f16f86fa52bd3ac1c6c79a8efd0ac65c
Abitova Dina <123065685+uadinaa@users.noreply.github.com>
2025-11-27 01:08:46 +0500

    Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,47 @@
-import { useAuth } from "../context/AuthContext";
-import { auth } from "../firebase";
-import { signOut } from "firebase/auth";
+// import { useAuth } from "../context/AuthContext";
+// import { auth } from "../firebase";
+// import { signOut } from "firebase/auth";
+// import { useNavigate } from "react-router-dom";
+// import "../styles/Profile.css"
+
+
+// export default function Profile() {
+//     const { user } = useAuth();
+//     const navigate = useNavigate();
+
+//     const logout = async () => {
+//         await signOut(auth);
+//         navigate("/login");
+//     };
+
+//     return (
+//         <div className="profile-container">
+//             <div className="profile-info">
+//                 <h2>Your Profile</h2>
+
+//                 <p><strong>Email:</strong> {user?.email}</p>
+
+//                 <button className="logout" onClick={logout}>Logout</button>
+//             </div>
+//         </div>
+//     );
+// }
+
+
+
+import {useDispatch, useSelector} from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../slices/authSlice.js";
 import "../styles/Profile.css"
 
 
 export default function Profile() {
-    const { user } = useAuth();
+    const user = useSelector((state) => state.auth.user);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const logout = async () => {
-        await signOut(auth);
+        await dispatch(logoutUser());
         navigate("/login");
     };
 
