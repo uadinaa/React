4e2dc7440c4b5e14b3028e1b4c2f0cfcdf7561cc
Abitova Dina <123065685+uadinaa@users.noreply.github.com>
2025-11-27 01:13:10 +0500

    ProtectedRoute

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,8 +1,20 @@
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "../context/AuthContext";
+
+// export default function ProtectedRoute({ children }) {
+//     const { user } = useAuth();
+
+//     if (!user) return <Navigate to="/login" replace />;
+
+//     return children;
+// }
+
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useSelector } from "react-redux";
 
 export default function ProtectedRoute({ children }) {
-    const { user } = useAuth();
+    const user = useSelector((state) => state.auth.user);
+
 
     if (!user) return <Navigate to="/login" replace />;
 
